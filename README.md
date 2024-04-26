b82feaaf66b34b19b0dba59440b2c522aa9f1a9b
nworb999 <emma.nworb@gmail.com>
2024-04-25 20:47:37 -0700

    Remove env

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # bad-party
+* no one at the party is listening to each other
+
+## characters
+* control which characters talk to which (one at a time with spectators at first only)
+
+## environment
+* maybe
+
+## content
+* topic generation
+* responses -- stream of consciousness
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from pythonosc import udp_client
+import os
+from dotenv import load_dotenv
 from pythonosc import osc_message_builder
+from bad_party.ollama import get_response as get_response
+from bad_party.ssh_tunnel import start_tunnel, stop_tunnel
+
+
+load_dotenv()
+
+imagination_ip = os.environ.get("IMAGINATION_IP")
+imagination_port = int(os.environ.get("IMAGINATION_PORT"))
+local_port = int(os.environ.get("LOCAL_PORT"))
+ssh_user = os.environ.get("SSH_USERNAME")
+ssh_keyfile = os.environ.get("SSH_KEYFILE")
 
 
 def get_utterance_from_llm(prompt):
-    return "Hello, this is a response from the LLM based on the prompt: " + prompt
+    return get_response(prompt)
 
 
 def send_osc_message(client, address, dialogue):
@@ -16,11 +29,11 @@ def send_osc_message(client, address, dialogue):
 
 def main():
     ip = "127.0.0.1"
-    port = 12345
+    port = 54321
 
     client = udp_client.SimpleUDPClient(ip, port)
 
-    prompt = "Start off a conversation in an unsettling way."
+    prompt = "Start off a conversation in a cringe, unsettling way but with an apathetic, uninterested tone."
 
     dialogue = get_utterance_from_llm(prompt)
 
@@ -28,4 +41,11 @@ def main():
 
 
 if __name__ == "__main__":
+    start_tunnel(
+        remote_server=imagination_ip,
+        ssh_username=ssh_user,
+        ssh_pkey=ssh_keyfile,
+        remote_port=imagination_port,
+        local_port=local_port,
+    )
     main()
