5e3bf10863f0bb7161daaba61ba11ef6533eb43e
nworb999 <emma.nworb@gmail.com>
2024-05-29 23:48:35 -0700

    Get into it

--- bad_party/langchain_sandbox.py
+++ bad_party/langchain_sandbox.py
@@ -1,7 +1,9 @@
+import warnings
+warnings.filterwarnings("ignore", message=r"`pydantic.error_wrappers:ValidationError` has been moved to `pydantic:ValidationError`.")
 import os
 import openai
 from langchain.prompts import ChatPromptTemplate
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.schema.output_parser import StrOutputParser
 from dotenv import load_dotenv, find_dotenv
 from langchain_openai import OpenAIEmbeddings
@@ -28,8 +30,53 @@ Question: {question}
 prompt = ChatPromptTemplate.from_template(template)
 
 chain = RunnableMap({
-    "context": lambda x: retriever.get_relevant_documents(x["question"]),
+    "context": lambda x: retriever.invoke(x["question"]),
     "question": lambda x: x["question"]
 }) | prompt | model | output_parser
 
-chain.invoke({"question": "where did harrison work?"})
+print(chain.invoke({"question": "where did harrison work?"}))
+
+
+functions = [
+    {
+      "name": "weather_search",
+      "description": "Search for weather given an airport code",
+      "parameters": {
+        "type": "object",
+        "properties": {
+          "airport_code": {
+            "type": "string",
+            "description": "The airport code to get the weather for"
+          },
+        },
+        "required": ["airport_code"]
+      }
+    },
+        {
+      "name": "sports_search",
+      "description": "Search for news of recent sport events",
+      "parameters": {
+        "type": "object",
+        "properties": {
+          "team_name": {
+            "type": "string",
+            "description": "The sports team to search for"
+          },
+        },
+        "required": ["team_name"]
+      }
+    }
+  ]
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("human", "{input}")
+    ]
+)
+model = ChatOpenAI(temperature=0).bind(functions=functions)
+
+runnable = prompt | model
+
+print(runnable.invoke({"input": "what is the weather in sf"}))
+
+print(runnable.invoke({"input": "how did the patriots do yesterday?"}))
